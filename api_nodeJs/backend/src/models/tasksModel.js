60b843c19dddbfa24858bee9a70ccc55cf44606b
Wagner torres <wvinicius.torres@gmail.com>
2024-03-09 19:07:18 -0400

    erro ao enviar parametro em taksmodel createtask

---
+++ api_nodeJs/backend/src/models/tasksModel.js
@@ -0,0 +1,19 @@
+const connection = require('./connection');
+
+const getAll = async () => {
+    const tasks = await connection.execute('SELECT * FROM tasks');
+    return tasks[0];
+};
+
+const createTask = async (task = undefined || {}) => {
+    const { title } = task;
+    const query = 'INSERT INTO tasks(title, status, created_at) VALUES (?, ?, ?)';
+    const dateUTC = new Date(Date.now()).toUTCString();
+    const createdTask = await connection.execute(query, [title, 'parado', dateUTC]);
+    return createdTask[0];
+}
+
+module.exports = {
+    getAll,
+    createTask
+};
